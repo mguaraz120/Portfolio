4a72c68c82bbdb387dd889989d59cda8fb79c8c5
Mario Vizcaino <vizhar1952@gmail.com>
2019-11-16 00:28:23 -0500

    finished css, working in the funcionality of the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,16 @@ $( document ).ready(function()
 
 $(".menu-toggler").on("click", function(){
     $(this).toggleClass("open");
-    $(".top-nav").toggleClass("open");
+    $(".top-nav").toggleClass("open");   
 });
-    
+$(".about").on("click", function(){
+    $(this).toggleClass("open");
+    $(".two").toggleClass("open");   
+});
+   const btnScrollToTop = document.querySelector("#btnScrollToTop");
+   btnScrollToTop.addEventListener("click", function(){
+       window.scrollTo(0, 0);
+       $("html, body").animate({scrollTo:0}, "slow"); 
+   })
+
 });
